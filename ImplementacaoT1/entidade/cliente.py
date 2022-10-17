cd45673a1f6a61f2378a23fa88d38d0cbb1362ee
lucaspasti <116039872+lucaspasti@users.noreply.github.com>
2022-10-17 20:52:32 -0300

    Add files via upload

---
+++ ImplementacaoT1/entidade/cliente.py
@@ -0,0 +1,30 @@
+class Cliente:
+
+    def __init__(self,nome: str, cpf: str, telefone: str):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__telefone = telefone
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @telefone.setter
+    def telefone(self, telefone: str):
+        self.__telefone = telefone
+
+    @cpf.setter
+    def cpf(self, cpf: str):
+        self.__cpf = cpf
